947f10f643cda512eda77c2197e2ad7ea19d31b4
Margaret <margaritasobcenko@gmail.com>
2025-01-13 21:15:28 +0200

    add all defs

---
+++ maneger_user.py
@@ -0,0 +1,36 @@
+import redis
+import bcrypt
+import json
+
+class Social_nets:
+    def add_user(self, username,password, fullname):
+        if self.redis_client.hexists("users", username):
+            print("Користувач вже існує")
+            return False
+        password = bcrypt.hashpw(password.encode("utf-8"),bcrypt.gensalt())
+        user_data = {"password":password,"full_name":fullname}
+
+        self.redis_client.hset("users",username,json.dumps(user_data))
+        return True
+        data = self.redis_client.hget("users", username)
+        json.loads(data)
+
+    def lig_in(self):
+        pass
+
+    def del_user(self):
+        pass
+
+    def update_user_info(self):
+        pass
+    def search_user(self):
+        pass
+
+    def viev_user(self): #fullname
+        pass
+
+    def user_friends(self):
+        pass
+
+    def user_publications(self):
+        pass
