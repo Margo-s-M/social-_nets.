59aef75e28069210ce7305c5766502d86612afcf
Margaret <margaritasobcenko@gmail.com>
2025-01-15 10:29:50 +0200

    add def del  update

--- maneger_user.py
+++ maneger_user.py
@@ -23,8 +23,26 @@ class Social_nets:
             return True
         print("Неправильний логін або пароль.")
         return False
-    def del_user(self):
-        pass
+
+    def del_user(self, username):
+        if not self.redis_client.hdel("users", username):
+            print("Користувач не знайдений.")
+            return False
+
+        print(f"Користувача {username} успішно видалено.")
+        return True
 
     def update_user_info(self):
-        pass
+        user_data = self.redis_client.hget("users", username)
+        if not user_data:
+            print("Користувач не знайдений.")
+            return False
+
+        user_data = json.loads(user_data)
+        if fullname:
+            user_data["full_name"] = fullname
+        if password:
+            user_data["password"] = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
+
+        self.redis_client.hset("users", username, json.dumps(user_data))
+        print(f"Інформація про користувача {username} успішно оновлена.")
